a9efb7f20ba46d8d837b48b46d590d5c27a7c316
TGriffey <tgriffey9389@gmail.com>
2015-08-21 12:51:06 -0500

    Created core-dependencies module

---
+++ javascripts/core-dependencies.js
@@ -0,0 +1,10 @@
+define(function (require) {
+
+    return function () {
+
+        var bootstrap = require("bootstrap"),
+            firebase = require("firebase"),
+            hbs = require("require-handlebars-plugin");
+
+    };
+});
